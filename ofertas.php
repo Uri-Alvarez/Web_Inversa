9f43a91e392a3201b20bbb8175192401883e6358
Uri-Alvarez <cdmx2974@amerike.edu.mx>
2025-05-06 11:45:51 -0600

    Metiendo pagina 2

---
+++ ofertas.php
@@ -0,0 +1,14 @@
+<!-- ofertas.php -->
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <title>Ofertas de Temu</title>
+  <link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+  <h1>¡Bienvenido a las Ofertas Especiales de Temu!</h1>
+  <p>Aquí encontrarás las mejores promociones.</p>
+  <a href="index.php">Volver al inicio</a>
+</body>
+</html>
