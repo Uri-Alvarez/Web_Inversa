8ce7799c9ded83bd72cdc00ed1d19cc945867bfc
Uri-Alvarez <cdmx2974@amerike.edu.mx>
2025-06-04 17:07:27 -0600

    1

--- conexion.js
+++ conexion.js
@@ -1,3 +1,5 @@
+<script type="module" src="conexion.js"></script>
+// conexion.js
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 <script src="https://unpkg.com/@supabase/supabase-js@2"></script>
 
